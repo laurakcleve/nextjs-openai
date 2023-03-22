795684f31be96d5f0b427804d4a1237bd82935e1
laura <laurakcleve@gmail.com>
2023-03-22 14:28:43 -0600

    add winston logging, change chunks to use name

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -7,6 +7,7 @@ import {
 import { Configuration, OpenAIApi } from 'openai'
 import path from 'path'
 import { promises as fs } from 'fs'
+import winston from 'winston'
 import type { CustomMessage } from '..'
 
 const configuration = new Configuration({
@@ -16,17 +17,27 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration)
 
+const { combine, timestamp, json } = winston.format
+const logger = winston.createLogger({
+  level: 'info',
+  format: combine(timestamp(), json()),
+  transports: [new winston.transports.File({ filename: 'main.log' })],
+})
+
 type IndexChunk = {
   content: string
-  slug: string
+  name: string
   vector: number[]
 }
 
 type Score = {
   score: number
   content: string
-  slug: string
+  name: string
 }
+
+const EMBEDDING_MODEL = 'text-embedding-ada-002'
+const CHAT_MODEL = 'gpt-3.5-turbo'
 
 const BASE_PROMPT =
   "You are a swashbuckling pirate, and a programmer who specializes in web development, and you only speak in pirate speak. You are the digital soul of Jimmy Cleveland, and you have a Youtube channel for which you are currently answering questions. All your answers should be in pirate speak, and try to format your answers in markdown. Give a succinct answer to the question using only the information in excerpt below, as if you have no prior knowledge about the question, and remember to speak like a pirate. Translate any text you use from the excerpt into pirate speak. If there is no information in the excerpt that is relevant to the question, respond with 'NOT FOUND', then apologize in pirate speak and say there isn't enough information in the blog to answer, in pirate speak."
@@ -39,9 +50,15 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<CustomMessage[]>
 ) {
+  logger.info({ 'req.body.userInput': req.body.userInput })
+
   const queryEmbedding = await openai.createEmbedding({
     input: req.body.userInput,
-    model: 'text-embedding-ada-002',
+    model: EMBEDDING_MODEL,
+  })
+  logger.info({
+    queryEmbedding: queryEmbedding.data.data[0],
+    queryEmbeddingModel: EMBEDDING_MODEL,
   })
 
   const queryVector = queryEmbedding.data.data[0].embedding
@@ -57,7 +74,7 @@ export default async function handler(
     return {
       score: similarity(queryVector, chunk.vector),
       content: chunk.content,
-      slug: chunk.slug,
+      name: chunk.name,
     }
   })
   scores.sort((a, b) => b.score - a.score)
@@ -74,13 +91,15 @@ export default async function handler(
     req.body.newChatFromUser.map((item: CustomMessage) => item.message)
 
   const completion = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
+    model: CHAT_MODEL,
     messages: formattedChat,
     temperature: 0.0,
   })
 
   const assistantMessage = completion.data.choices[0].message
   const assistantMessageContent = assistantMessage?.content || ''
+
+  logger.info({ response: assistantMessage, responseModel: CHAT_MODEL })
 
   const re = /\s*NOT FOUND[^\w]\s*/
   let sourceFound = !re.test(assistantMessageContent)
@@ -91,7 +110,7 @@ export default async function handler(
     {
       id: completion.data.created,
       message: { ...assistantMessage, content: strippedMessage },
-      source: sourceFound ? scores[0].slug : null,
+      source: sourceFound ? scores[0].name : null,
     },
   ]
 
